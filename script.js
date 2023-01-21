b0c999901eb456226e8f03df08a479fd278e5a85
Allyson Thales <allysonthales@protonmail.com>
2023-01-21 12:45:59 -0300

    aula 04 - salvando evento localStorage

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const form = document.querySelector("#forms-habits");
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector("header button");
 
-const data = {
-  run: ["01-03", "01-11", "01-14"],
-  water: ["01-03", "01-04", "01-10", "01-12", "01-14"],
-  gym: ["01-07", "01-09", "01-10", "01-13", "01-14"],
-  sleep: ["01-06", "01-09", "01-10", "01-11"],
-  walker: ["01-04", "01-07", "01-10", "01-11", "01-12", "01-14"],
-  diet: ["01-07", "01-09", "01-13", "01-14"],
-};
+button.addEventListener("click", add);
+form.addEventListener("change", save);
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, 5);
+
+  const dayExists = nlwSetup.dayExists(today);
+
+  if (dayExists) {
+    alert("Dia ja existe");
+    return;
+  }
+  nlwSetup.addDay(today);
+}
+
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data));
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {};
 
 nlwSetup.setData(data);
-
 nlwSetup.load();
